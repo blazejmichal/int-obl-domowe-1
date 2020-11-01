70bf74c279a31b1ceb6530a2771af63cf260c772
Blazej <Blazej@DESKTOP-P8SE49K>
2020-11-01 03:14:55 +0200

    Poprawki brute force

--- BackpackBruteForce.py
+++ BackpackBruteForce.py
@@ -1,4 +1,3 @@
-from Item import Item
 import matplotlib.pyplot as plt
 import itertools
 import timeit
@@ -18,25 +17,34 @@ class BackpackBruteForce:
     @staticmethod
     def run(ITEMS, CAPACITY):
         start = timeit.default_timer()
-        BackpackBruteForce.getMaxValue(ITEMS, CAPACITY)
+        bestSet = BackpackBruteForce.getBestSet(ITEMS, CAPACITY)
+        maxValue = BackpackBruteForce.getSetValue(bestSet)
         stop = timeit.default_timer()
         time = stop - start
         print("Czas dla BruteForce: " + str(time))
+        print("Max wartosc dla BruteForce: " + str(maxValue))
         return time
 
     @staticmethod
-    def getMaxValue(ITEMS, CAPACITY):
+    def getBestSet(ITEMS, CAPACITY):
         xIteration = []
         yValue = []
         iteration = 0
+        matchingSets = []
         for i in range(0, len(ITEMS) + 1):
             for subSet in itertools.combinations(ITEMS, i):
-                subSetValues = map(lambda item: item.value, subSet)
-                totalSubSetValue = sum(subSetValues)
+                subSetValue = BackpackBruteForce.getSetValue(subSet)
+                subSetWeights = map(lambda item: item.weight, subSet)
+                subSetWeight = sum(subSetWeights)
                 iteration += 1
-                xIteration.append(iteration)
-                yValue.append(totalSubSetValue)
-                if (totalSubSetValue == 1630):
-                    BackpackBruteForce.plotChart(xIteration, yValue)
-                    return totalSubSetValue
-        return 0
+                if (subSetWeight <= CAPACITY):
+                    xIteration.append(iteration)
+                    yValue.append(subSetValue)
+                    matchingSets.append(subSet)
+        BackpackBruteForce.plotChart(xIteration, yValue)
+        return max(matchingSets, key=lambda matchingSet: BackpackBruteForce.getSetValue(matchingSet))
+
+    @staticmethod
+    def getSetValue(set):
+        setValues = map(lambda item: item.value, set)
+        return sum(setValues)
